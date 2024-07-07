5800c26d014aa543ef1c37fad217d8532a494231
Ayush Satpathy <137210906+Ayush-5k@users.noreply.github.com>
2024-07-07 22:25:38 +0530

    Add files via upload

---
+++ ValidParentheses.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class ValidParentheses {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch: s.toCharArray())
+        {
+            switch (ch)
+            {
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(ch);
+                    break;
+                case ')':
+                    if(stack.isEmpty() || stack.pop() != '(')
+                    {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if(stack.isEmpty() || stack.pop() != '{')
+                    {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if(stack.isEmpty() || stack.pop() != '[')
+                    {
+                        return false;
+                    }
+                    break;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
