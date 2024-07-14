74df29884517ad1afeb444f385cd68c71b1e2102
Ayush Satpathy <137210906+Ayush-5k@users.noreply.github.com>
2024-07-14 23:34:33 +0530

    Add files via upload

---
+++ ReverseBits.java
@@ -0,0 +1,12 @@
+public class ReverseBits {
+    public int reverseBits(int n) {
+        int ans = 0;
+        for(int i=0;i<32;i++){
+            int lsb = n & 1 ;
+            int temp= lsb<<(31-i);
+            ans = ans|temp;
+            n = n>>1;
+        }
+        return ans;
+    }
+}
